c2f9e7259bf4d915cc667f48bc493bc8570cfdd5
Icarus <520icarus1314@gmail.com>
2022-03-02 09:28:51 +0800

    init project

---
+++ src/SysInfo.js
@@ -0,0 +1,85 @@
+import { NGrid, NGi, NProgress } from "naive-ui";
+import { getQueryParam } from "@utils/url.js";
+
+export default {
+  components: {
+    NGrid,
+    NGi,
+    NProgress,
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      machineId: getQueryParam("machine_id"),
+      cpuUsed: 0,
+      memUsed: 0,
+      memTotal: 0,
+      swapUsed: 0,
+      swapTotal: 0,
+    };
+  },
+  methods: {
+    getCpuUsed() {
+      fetch("http://127.0.0.1:32123/ssh/assist", {
+        method: "POST",
+        body: JSON.stringify({
+          id: 1 * this.machineId,
+          cmd: "vmstat",
+        }),
+      }).then((res) => {
+        res.json().then((json) => {
+          json = json.data.split("\n")[2].split(" ");
+          trim(json, "");
+          this.cpuUsed = (100 - json[14]).toFixed(2);
+        });
+      });
+    },
+    getMemUsed() {
+      fetch("http://127.0.0.1:32123/ssh/assist", {
+        method: "POST",
+        body: JSON.stringify({
+          id: 1 * this.machineId,
+          cmd: "free",
+        }),
+      }).then((res) => {
+        res.json().then((json) => {
+          json = json.data.split("\n");
+          let mem = json[1].split(" ");
+          let swap = json[2].split(" ");
+          trim(mem, "");
+          trim(swap, "");
+          console.log(mem, swap);
+          this.memTotal = mem[1];
+          this.memUsed = mem[2];
+          this.swapTotal = swap[1];
+          this.swapUsed = swap[2];
+        });
+      });
+    },
+  },
+  mounted() {
+    this.getCpuUsed();
+    this.getMemUsed();
+    setInterval(() => {
+      this.getCpuUsed();
+      this.getMemUsed();
+    }, 3000);
+  },
+};
+
+function getQueryParam(name) {
+  return decodeURIComponent((new RegExp("[?|&]" + name + "=" + "([^&;]+?)(&|#|;|$)").exec(location.href) || [, ""])[1].replace(/\+/g, "%20")) || null;
+}
+
+function trim(arr, target) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] == target) {
+      arr.splice(i, 1);
+    }
+  }
+}
---
+++ src/SysInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <NGrid
+    cols="24"
+    style="padding: 0 10px; width: calc(100% - 20px); text-align: center; padding-top: 20px;"
+  >
+    <NGi span="8">CPU</NGi>
+    <NGi span="8">内存</NGi>
+    <NGi span="8">交换</NGi>
+    <NGi span="8">
+      <NProgress
+        type="circle"
+        :percentage="cpuUsed"
+        :height="20"
+        :border-radius="4"
+        :fill-border-radius="0"
+      />
+    </NGi>
+    <NGi span="8">
+      <NProgress
+        type="circle"
+        :percentage="Math.round(memUsed / memTotal * 100)"
+        :height="20"
+        :border-radius="4"
+        :fill-border-radius="0"
+      />
+    </NGi>
+    <NGi span="8">
+      <NProgress
+        type="circle"
+        :percentage="Math.round(swapUsed / swapTotal * 100)"
+        :height="20"
+        :border-radius="4"
+        :fill-border-radius="0"
+      />
+    </NGi>
+    <!-- <NGi span="8"></NGi> -->
+    <NGi
+      span="8"
+      offset="8"
+    >{{ Math.round(memUsed / 1024) }} / {{ Math.round(memTotal / 1024) }} (MB)</NGi>
+    <NGi span="8">{{ Math.round(swapUsed / 1024) }} / {{ Math.round(swapTotal / 1024) }} (MB)</NGi>
+  </NGrid>
+</template>
+
+<script src="./SysInfo.js"></script>
+
+<style src="./SysInfo.css"></style>
